8b7ea8b3a3b7ca71d82a676618e87af849feebf5
SleepyWolfy <inaki.2012@hotmail.com>
2021-11-11 12:24:40 -0300

    Lets try

--- Code/app/webpack.config.js
+++ Code/app/webpack.config.js
@@ -11,5 +11,5 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
   ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true, historyApiFallback: true },
 };
