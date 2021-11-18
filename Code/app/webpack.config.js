5a1728226c608f7e4195e1591bfae99fc745a6ca
SleepyWolfy <inaki.2012@hotmail.com>
2021-11-18 12:08:31 -0300

    Trying to make this work.

--- Code/app/webpack.config.js
+++ Code/app/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: 'development',
-  entry: "./src/index.js",
+  entry: ["./src/index.js", "./src/style.scss"],
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
@@ -12,4 +13,51 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'bundle.css',
+						},
+					},
+					{ loader: 'extract-loader' },
+					{ loader: 'css-loader' },
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									autoprefixer()
+								]
+							}
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							// Prefer Dart Sass
+							implementation: require('sass'),
+
+							// See https://github.com/webpack-contrib/sass-loader/issues/804
+							webpackImporter: false,
+							sassOptions: {
+								includePaths: ['./node_modules'],
+							},
+						},
+					}
+				],
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env'],
+				},
+			}
+		],
+	},
 };
