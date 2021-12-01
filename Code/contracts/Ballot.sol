6731547f8aad3498938baf4fb3a203d9a7b4cde1
SleepyWolfy <inaki.2012@hotmail.com>
2021-12-01 18:36:09 -0300

    Implemented get all ballots.

--- Code/contracts/Ballot.sol
+++ Code/contracts/Ballot.sol
@@ -7,5 +7,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Ballot is Ownable{
     Structs.BallotData private _myData;    
 
-    
+    function GetData() onlyOwner public returns (Structs.BallotData){
+        return _myData;
+    }
+
 }
--- Code/contracts/EventVoterManager.sol
+++ Code/contracts/EventVoterManager.sol
@@ -4,7 +4,23 @@ pragma solidity 0.8.0;
 import "./Ballot.sol";
 
 contract EventVoterManager {
-    mapping(address => Ballot) _ballotsMatchedToOwners;
+    mapping(address => Ballot) private _ballotsMatchedToOwners;
+    address[] private _addressMappingArray;
+
+    event ReturnAllBallots(Structs.BallotData[] ballots);
+
     constructor(){
+    }
+
+    function GetAllBallots() public view
+    {
+        Structs.BallotData[] allData = new Structs.BallotData[](_addressMappingArray.length);
+
+        for(uint i = 0; i < _addressMappingArray.length; i++)
+        {
+            allData.push(_ballotsMatchedToOwners[_addressMappingArray[i]].GetData());
+        }
+
+        emit ReturnAllBallots(allData);
     }
 }
