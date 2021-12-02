2bbb650af9d15ebc7bb082ba7a07c14be271414d
SleepyWolfy <inaki.2012@hotmail.com>
2021-12-02 13:43:31 -0300

    Added tests for Add ballots.

--- Code/test/eventtoken.js
+++ Code/test/eventtoken.js
@@ -3,7 +3,7 @@ const EventToken = artifacts.require("EventToken");
 contract("EventToken", accounts => {
   it("should put 10000000 EventToken in the first account", async () => {
     const instance = await EventToken.deployed();
-    const balance = await instance.getBalance(accounts[0]).call();
-    assert.equal(balance.valueOf(), 10000000, "10000000 wasn't in the first account");
+    const balance = await instance.getBalance(accounts[0]);
+    assert.equal(balance.valueOf(), 10000000 * 10**18, "10000000000000000000000000 wasn't in the first account");
   });
 });
--- Code/test/eventvotetests.js
+++ Code/test/eventvotetests.js
@@ -1,11 +1,30 @@
 const EventVoterManager = artifacts.require("EventVoterManager");
 
 contract("EventVoterManager", accounts => {
-  it("Add ballot should add", async () => {
-    const instance = await EventVoterManager.deployed();
-    const from = (long)(Date.now()/1000);
-    await instance.AddBallot(accounts[0], "Vote1", from, 299);
-    const allBallots = await instance.GetAllBallots(); 
-    assert.equal(allBallots.length, 1, "Length should have been 1");
-  });
+    let instance = null;
+    beforeEach(async () => {
+        var instance1 = await EventVoterManager.deployed();
+        assert.ok(instance1)
+
+        instance = await EventVoterManager.new(instance1.address);
+
+    });
+
+    it("Add ballot should add", async () => {
+        let date = (new Date()).getTime();
+        const from = Number.parseInt(date/1000);
+        await instance.AddBallot(accounts[0], "Vote1", from, 299);
+        const allBallots = await instance.GetAllBallots(); 
+        assert.equal(allBallots.length, 1, "Length should have been 1");
+    });
+
+    it("Three Add ballot should add three times", async () => {
+        let date = (new Date()).getTime();
+        const from = Number.parseInt(date/1000);
+        await instance.AddBallot(accounts[0], "Vote1", from, 299);
+        await instance.AddBallot(accounts[3], "Vote2", from, 20);
+        await instance.AddBallot(accounts[2], "Vote3", from, 11);
+        const allBallots = await instance.GetAllBallots(); 
+        assert.equal(allBallots.length, 3, "Length should have been 3");
+    });
 });
