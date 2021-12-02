9b74fa7b262519b509880c90b554372f206ba09e
Matias González <matias.gonzalez.2407@gmail.com>
2021-12-02 17:22:14 -0300

    buy and sell functional tests

--- Code/test/eventtoken.js
+++ Code/test/eventtoken.js
@@ -10,64 +10,39 @@ contract("EventToken", accounts => {
 
   });
 
-  /*
-  it("should put 10000000 EventToken in the first account", async () => {
-
-    const balance = await instance.getBalance.call(accounts[0]);
-    assert.equal(balance.valueOf(), 10000000, "10000000 wasn't in the first account");
-  });
-
-  it("should call a function that depends on a linked library", async () => {
-
-    const EventTokenBalance = await instance.getBalance.call(accounts[0]);
-    const EventTokenBalanceInEth = await instance.getBalanceInEth.call(
-      accounts[0],
-    );
-
-    const expected = 2 * EventTokenBalance.toNumber();
-
-    assert.equal(
-      EventTokenBalanceInEth.toNumber(),
-      expected,
-      "Library function returned unexpeced function, linkage may be broken",
-    );
-  });
-
-  it("should send coin correctly", async () => {
-
-    const account1 = accounts[0];
-    const account2 = accounts[1];
-
-    // get initial balances
-    const initBalance1 = await instance.getBalance.call(account1);
-    const initBalance2 = await instance.getBalance.call(account2);
-
-    // send coins from account 1 to 2
-    const amount = 10;
-    await instance.sendCoin(account2, amount, { from: account1 });
-
-    // get final balances
-    const finalBalance1 = await instance.getBalance.call(account1);
-    const finalBalance2 = await instance.getBalance.call(account2);
-
-    assert.equal(
-        finalBalance1,
-        initBalance1.toLong() - amount,
-      "Amount wasn't correctly taken from the sender",
-    );
-    assert.equal(
-      finalBalance2.toNumber(),
-      initBalance2.toNumber() + amount,
-      "Amount wasn't correctly sent to the receiver",
-    );
-  });
-*/
   
-  it("should buy 10Eths worth in tokens from the main account", async () => {
+  it("should buy 100 tokens from the 2nd account", async () => {
 
     await instance.buyTokens(100, {from: accounts[1], value: 1*10**18});
-    const balance = instance.getBalance.call(accounts[1]);
-    assert.equal(balance.valueOf(), 100, "100 wasn't in the second account");
+    const balance = await instance.getBalance.call(accounts[1]) / 10**18;
+
+    assert.equal(parseInt(balance), 100, "100 wasn't in the second account");
+  });
+
+  it("should remove 100 tokens from the main acc", async () => {
+
+    const originalBalance = await instance.getBalance.call(accounts[0]) / 10**18;
+    await instance.buyTokens(100, {from: accounts[1], value: 1*10**18});
+    const mainBalance = await instance.getBalance.call(accounts[0]) / 10**18;
+    assert.equal(parseInt(mainBalance), parseInt(originalBalance) - 100, "100 wasn't in the second account");
+  });
+
+  it("should remove 50 tokens from the 2nd account", async () => {
+
+    await instance.buyTokens(100, {from: accounts[1], value: 1*10**18});
+    const originalBalance = await instance.getBalance.call(accounts[1]) / 10**18;
+    await instance.sellTokens(50,{from: accounts[1]});
+    const mainBalance = await instance.getBalance.call(accounts[1]) / 10**18;
+    assert.equal(parseInt(mainBalance), parseInt(originalBalance) - 50, "50 werent removed in the second account");
+  });
+
+  it("should add 50 tokens to the main acc", async () => {
+
+    await instance.buyTokens(100, {from: accounts[1], value: 1*10**18});
+    const originalBalance = await instance.getBalance.call(accounts[0]) / 10**18;
+    await instance.sellTokens(50,{from: accounts[1]});
+    const mainBalance = await instance.getBalance.call(accounts[0]) / 10**18;
+    assert.equal(parseInt(mainBalance), parseInt(originalBalance) + 50, "50 tokens werent added to main account");
   });
 
 
